91a159018b1cd0fe658a8e200a3e970eef7d9eb7
SheinSlava <slav1195@yandex.ru>
2023-04-27 22:51:41 +0300

    Get predict (debug)

--- src_class_2/predict.py
+++ src_class_2/predict.py
@@ -18,22 +18,26 @@ def parse_arguments() -> argparse.Namespace:
 
     return parser.parse_args()
 def processing_spectogramm(pred_dir):
+    res = []
     arr_item = np.load(pred_dir)
     plt.imsave('pred.jpg', arr_item.T)
     im = Image.open('pred.jpg')
     im = im.resize((800, 80), Image.LANCZOS)
     im = np.array(im)
-
-    return im
+    res.append(im)
+    res = np.array(res)
+    return res
 
 if __name__ == "__main__":
 
-    model_dir = '/home/sheins/z2_gz/models/my_model2.h5'
+    model_dir = '/home/sheins/z2_gz/src_class_2/models/my_model2.h5'
+
 
     arguments = parse_arguments()
     mel_diagram_dir = arguments.file_dir
 
     process_mel = processing_spectogramm(mel_diagram_dir)
+
 
     model = tf.keras.models.load_model(model_dir)
     predictions = model.predict(process_mel)
@@ -41,9 +45,8 @@ if __name__ == "__main__":
 
     print("Results: ", predictions)
 
-
-
-    # print(
-    #     "This diagramm most likely belongs to {} with a {:.2f} percent confidence."
-    #     .format(class_names[np.argmax(score)], 100 * np.max(score))
-    # )
+    class_names = ['clear', 'noisy']
+    print(
+        "This diagramm most likely belongs to {} with a {:.2f} percent confidence."
+        .format(class_names[np.argmax(score)], 100 * np.max(score))
+    )
