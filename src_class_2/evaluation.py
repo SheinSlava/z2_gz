3d0e99ffe2496c8e24be35ef5713ab6f58dfb09f
SheinSlava <slav1195@yandex.ru>
2023-04-28 01:04:00 +0300

    F1 macro eval on val data (0.9597)

---
+++ src_class_2/evaluation.py
@@ -0,0 +1,46 @@
+from predict import processing_spectogramm
+import tensorflow as tf
+import glob
+import numpy as np
+from sklearn.metrics import f1_score
+
+
+if __name__ == "__main__":
+
+    model_dir = '/home/sheins/z2_gz/src_class_2/models/my_model3.h5'
+    model = tf.keras.models.load_model(model_dir)
+    print(model.summary())
+
+    clear_dir = '/home/sheins/z2_gz/dataset/val/val/clean/*/*'
+    clear_mel = processing_spectogramm(clear_dir)
+    print(len(clear_mel))
+
+    noise_dir = '/home/sheins/z2_gz/dataset/val/val/noisy/*/*'
+    noisy_mel = processing_spectogramm(noise_dir)
+    print(len(noisy_mel))
+
+    y_clear = [0] * len(clear_mel)
+    y_noise = [1] * len(noisy_mel)
+    y = y_clear + y_noise
+
+    class_names = [0, 1]
+
+    prediction_cl = model.predict(clear_mel)
+    results_cl = []
+    for i in prediction_cl:
+        score = tf.nn.softmax(i)
+        score_value_cl = class_names[np.argmax(score)]
+        results_cl.append(score_value_cl)
+
+    predictions_no = model.predict(noisy_mel)
+    results_no = []
+    for i in predictions_no:
+        score = tf.nn.softmax(i)
+        score_value_no = class_names[np.argmax(score)]
+        results_no.append(score_value_no)
+
+    all_results = results_cl + results_no
+
+    print("F1 MACRO: ", f1_score(all_results, y, average='macro'))
+    # print("F1 MICRO: ", f1_score(all_results, y, average='micro'))
+    # print("F1: ", f1_score(all_results, y, average='macro'))
