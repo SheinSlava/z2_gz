9acc0c0aec92b417526d917f48589ca96b7a9794
SheinSlava <slav1195@yandex.ru>
2023-04-28 13:03:02 +0300

    Train denoising model

---
+++ scr_denoising/model.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import os
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+
+def my_model(num_classes=2, img_height=80, img_width=800):
+
+    model = Sequential([
+        layers.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.Huber(),
+                  metrics=['mse'])
+
+    return model
+
+
+def model_fit(model, train_X, test_X, train_Y, test_Y, checkpoint_path, epochs=10):
+    #     checkpoint_path = "training_1/cp.ckpt"
+    # checkpoint_path = "hys_res_1/cp-{epoch:04d}.ckpt"
+
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                     save_weights_only=True,
+                                                     verbose=1)
+
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                     save_weights_only=True,
+                                                     verbose=1)
+
+    history = model.fit(train_X,
+                        train_Y,
+                        epochs=10,
+                        validation_data=(test_X, test_Y),
+                        validation_steps=30,
+                        callbacks=[cp_callback])
+    os.listdir(checkpoint_dir)
+    return history
