067992b0b45acd465fe9173c235a92f24a9e6671
SheinSlava <slav1195@yandex.ru>
2023-05-01 02:48:00 +0300

    Debug

--- src_class_2/models/download_model_classify.sh
+++ src_class_2/models/download_model_classify.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-wget https://disk.yandex.ru/d/U_u_2T7jVIx1bg/my_model3.h5
+#wget https://disk.yandex.ru/d/U_u_2T7jVIx1bg/my_model3.zip
+
+
+HREF=$(curl -G "https://cloud-api.yandex.net/v1/disk/public/resources/download" --data-urlencode "public_key=https://disk.yandex.ru/d/_zIeyEK-q3IayA" | grep -E -o 'https:[^"]*')
+TARNAME="model_classify"
+wget -O $TARNAME $HREF
+
+unzip $TARNAME
+rm $TARNAME
